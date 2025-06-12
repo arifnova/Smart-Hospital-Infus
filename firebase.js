eeb9b65fa0d0553874e08ea8265704e7f6ba97c9
arifnova <arifnova258@gmail.com>
2025-06-13 00:11:38 +0700

    updatee

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHAS4YStMODoP1F3ZmoldNzz8fNMOgL4Y",
-  authDomain: "smart-hospital-93a43.firebaseapp.com",
-  databaseURL:
-    "https://smart-hospital-93a43-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smart-hospital-93a43",
-  storageBucket: "smart-hospital-93a43.firebasestorage.app",
-  messagingSenderId: "598927292000",
-  appId: "1:598927292000:web:1ddcdf768c2bd70604b505",
+  apiKey: "AIzaSyA7O-u401s2Sa3mcWkGfNHqdN2MxZ7dnqc",
+  authDomain: "gedung-cerdas.firebaseapp.com",
+  databaseURL: "https://gedung-cerdas-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gedung-cerdas",
+  storageBucket: "gedung-cerdas.firebasestorage.app",
+  messagingSenderId: "1079895536024",
+  appId: "1:1079895536024:web:b488f64c7de883e570bbed",
+  measurementId: "G-KVPQT6H2B6"
 };
 
 const app = initializeApp(firebaseConfig);
